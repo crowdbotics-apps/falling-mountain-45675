d6f13eddb370baccb9ba97d51dead090832fa10b
Crowdbotics <team@crowdbotics.com>
2024-01-29 20:14:35 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "falling-mountain-45675",
+  url: "https://falling-mountain-45675.botics.co",
+  api: "https://falling-mountain-45675.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
